24f7b90cce31366e015e1f6c4befbc6208619797
Artemisia <96399703+artemisia003@users.noreply.github.com>
2021-12-20 23:37:49 -0700

    add plot

---
+++ 2021-12-14-SpiceGirls/2021-12-14.R
@@ -0,0 +1,17 @@
+
+#------------------
+# Load packages
+#------------------
+library(tidyverse)
+
+
+
+#------------------
+# Import data
+#------------------
+lyrics <-  read.csv("https://github.com/jacquietran/spice_girls_data/raw/main/data/lyrics.csv", header = TRUE)
+
+studio_album_tracks <-  read.csv("https://github.com/jacquietran/spice_girls_data/raw/main/data/studio_album_tracks.csv", header = TRUE)
+
+
+
